cc72edc1d6760cf7c2844a94d7b30add22c5da66
Florry <viktor@frostdigital.se>
2017-06-01 17:45:23 +0200

    adds config option for logging timestamps

--- conf.js
+++ conf.js
@@ -1,20 +1,23 @@
 module.exports = {
   // Applications log level (error|warn|info|debug|silly)
-  logLevel: parseLogLevel(process.env.LOG_LEVEL) || 'debug',
+  logLevel: parseLogLevel(process.env.LOG_LEVEL) || 'debug',
 
   // Syslog host and port, if any
   // Example: `localhost:5499`
   syslog: process.env.SYSLOG || null,
-  
+
   // Name of syslog
   syslogName: process.env.SYSLOG_NAME || 'fruster noname',
-  
+
   // Syslog program name
-  syslogProgram: process.env.SYSLOG_PROGRAM || process.env.DEIS_APP || 'default'
+  syslogProgram: process.env.SYSLOG_PROGRAM || process.env.DEIS_APP || 'default',
+
+  timestamps: process.env.LOG_TIMESTAMPS === "true" || false
+
 };
 
 function parseLogLevel(str) {
-  if(str) {
+  if (str) {
     str = str.toLowerCase();
   }
   // align log level naming so `trace` becomes `silly
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,14 +1,34 @@
 const log = require('../index');
 
 describe('Fruster log', () => {
-  
-  it('should log a json object', () => {   
-    log.info('A JSON object', { 
-      foo: 1, 
-      bar: { 
-        a: 1, 
+
+  it('should info log a json object', () => {
+    log.info('Info: A JSON object', {
+      foo: 1,
+      bar: {
+        a: 1,
         b: 2
-      } 
+      }
+    }, 'yeah!');
+  });
+
+  it('should debug log a json object', () => {
+    log.debug('Debug: A JSON object', {
+      foo: 1,
+      bar: {
+        a: 1,
+        b: 2
+      }
+    }, 'yeah!');
+  });
+
+  it('should error log a json object', () => {
+    log.error('Error: A JSON object', {
+      foo: 1,
+      bar: {
+        a: 1,
+        b: 2
+      }
     }, 'yeah!');
   });
 
